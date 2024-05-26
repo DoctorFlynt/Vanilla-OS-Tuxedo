b82e6f2aa3599e1101d8a0fdbe75c0eb02df1c98
DoctorFlynt <169937838+DoctorFlynt@users.noreply.github.com>
2024-05-26 21:42:21 +0200

    install-debs.sh

--- includes.container/deb-pkgs/install-debs.sh
+++ includes.container/deb-pkgs/install-debs.sh
@@ -18,6 +18,24 @@ for file in /deb-pkgs/*; do
    echo "installing $file"
 
     apt-get install -y $file
+done
 
-    apt-get install -y tuxedo-control-center_2.1.8_amd64.deb
+set -e
+for file in /deb-pkgs/*; do
+    if [ ! -f "$file" ]; then
+        continue
+    fi
+
+    extension=${file##*.}
+    if [ $extension != "deb" ]; then
+        continue
+    fi
+
+    if [ "$file" = *"control-center"* ];then
+       continue
+    fi
+
+   echo "installing $file"
+
+    apt-get install -y $file
 done
