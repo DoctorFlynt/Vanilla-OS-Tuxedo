3f00025960f10022d30495ce9bdda34fa04b4194
DoctorFlynt <169937838+DoctorFlynt@users.noreply.github.com>
2024-05-26 22:02:06 +0200

    install-debs.sh

--- includes.container/deb-pkgs/install-debs.sh
+++ includes.container/deb-pkgs/install-debs.sh
@@ -22,22 +22,22 @@ for file in /deb-pkgs/*; do
     apt-get install -y $file
 done
 
-#set -e
-#for file in /deb-pkgs/*; do
-#    if [ ! -f "$file" ]; then
-#        continue
-#    fi
-#
-#    extension=${file##*.}
-#    if [ $extension != "deb" ]; then
-#        continue
-#    fi
-#
-#    if [ "$file" = *"control-center"* ];then
-#       continue
-#    fi
-#
-#   echo "installing $file"
-#
-#    apt-get install -y $file
-#done
+set -e
+for file in /deb-pkgs/*; do
+    if [ ! -f "$file" ]; then
+        continue
+    fi
+
+    extension=${file##*.}
+    if [ $extension != "deb" ]; then
+        continue
+    fi
+
+    if [ "$file" = *"control-center"* ];then
+       continue
+    fi
+
+   echo "installing $file"
+
+    apt-get install -y $file
+done
