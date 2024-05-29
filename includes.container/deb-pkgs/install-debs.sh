0bff4dbdc64f89fb637ee75992321d45f597298f
DoctorFlynt <169937838+DoctorFlynt@users.noreply.github.com>
2024-05-29 12:41:13 +0200

    Update install-debs.sh

--- includes.container/deb-pkgs/install-debs.sh
+++ includes.container/deb-pkgs/install-debs.sh
@@ -1,41 +1,22 @@
 #!/bin/bash
 
-apt-get install -y /deb-pkgs/tuxedo-keyboard_3.2.10_all.deb
+#apt-get install -y /deb-pkgs/tuxedo-keyboard_3.2.10_all.deb
 #apt-get install -y /deb-pkgs/tuxedo-keyboard-ite_0.4.3_all.deb
-
-set -e
-for file in /deb-pkgs/*; do
-    if [ ! -f "$file" ]; then
-        continue
-    fi
-
-    extension=${file##*.}
-    if [ $extension != "deb" ]; then
-        continue
-    fi
-
-    echo "installing $file"
-
-    apt-get install -y $file
-done
-
 
 #set -e
 #for file in /deb-pkgs/*; do
 #    if [ ! -f "$file" ]; then
 #        continue
 #    fi
-#
+
 #    extension=${file##*.}
 #    if [ $extension != "deb" ]; then
 #        continue
 #    fi
-#
-#    if [ "$file" = *"control-center"* ];then
-#       continue
-#    fi
-#
-#   echo "installing $file"
-#
+
+#    echo "installing $file"
+
 #    apt-get install -y $file
 #done
+
+
